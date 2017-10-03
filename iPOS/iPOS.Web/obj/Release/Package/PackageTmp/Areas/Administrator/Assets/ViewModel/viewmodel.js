44f884d590f60fb8de97e5cf06625e16709fc7fa
marky3heat <marky3heat@gmail.com>
2017-10-03 09:17:45 +0800

    10/3/2017

--- iPOS/iPOS.Web/obj/Release/Package/PackageTmp/Areas/Administrator/Assets/ViewModel/viewmodel.js
+++ iPOS/iPOS.Web/obj/Release/Package/PackageTmp/Areas/Administrator/Assets/ViewModel/viewmodel.js
@@ -4,6 +4,8 @@
 
     // #region CONTROLS                
     var isListShowed = ko.observable(true);
+    var isCreateModeShow = ko.observable(false);
+    var title = ko.observable("");
 
     // #endregion
 
@@ -16,114 +18,98 @@
 
     function loadTransactionList() {
         $("#transactionTable").dataTable().fnDestroy();
-        var $datatablesFixedheader = $('#transactionTable');
-        if ($datatablesFixedheader.length) {
-            $datatablesFixedheader = $datatablesFixedheader.DataTable({
-                deferRender: true,
-                scrollY: 370,
-                scrollCollapse: true,
-                scroller: true,
-                responsive: true,
-                dom: "<'row'<'col-sm-6'i><'col-sm-6'f>>" + "<'row'<'col-sm-12'tr>>" + "<'row'<'col-sm-6'l><'col-sm-6'p>>",
-                language: {
-                    paginate: {
-                        previous: '&laquo;',
-                        next: '&raquo;'
-                    },
-                    search: "_INPUT_",
-                    searchPlaceholder: "Search…"
+        var $datatables = $('#transactionTable');
+        $datatables.DataTable({
+            dom: "<'row'<'col-sm-6'i><'col-sm-6'f>>" + "<'table-responsive'tr>" + "<'row'<'col-sm-6'l><'col-sm-6'p>>",
+            language: {
+                paginate: {
+                    previous: '&laquo;',
+                    next: '&raquo;'
                 },
-                ajax: {
-                    url: RootUrl + "/Administrator/PawnshopTransactions/GetTransactions",
-                    type: "GET",
-                    datatype: "json"
-                },
-                columns: [
-                { data: "TransactionNo", "className": "text-left" },
-                {
-                    data: "TransactionDate",
-                    className: "text-left",
-                    render: function (data, type, row) {
-                        var pattern = /Date\(([^)]+)\)/;
-                        var results = pattern.exec(row.TransactionDate);
-                        var dt = new Date(parseFloat(results[1]));
-                        return (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear();
+                search: "_INPUT_",
+                searchPlaceholder: "Search…"
+            },
+            ajax: {
+                url: RootUrl + "/Administrator/Transactions/GetTransactions",
+                type: "GET",
+                datatype: "json"
+            },
+            columns: [
+            { data: "TransactionNo", "className": "text-left" },
+            {
+                data: "TransactionDate",
+                className: "text-left",
+                render: function (data, type, row) {
+                    var pattern = /Date\(([^)]+)\)/;
+                    var results = pattern.exec(row.TransactionDate);
+                    var dt = new Date(parseFloat(results[1]));
+                    return (dt.getMonth() + 1) + "/" + dt.getDate() + "/" + dt.getFullYear();
+                }
+            },
+            { data: "TransactionType", "className": "text-left" },
+            {
+                className: "text-left",
+                render: function (data, type, row) {
+                    return row.first_name + " " + row.last_name
+                }
+            },
+            {
+                data: "Status",
+                className: "text-center",
+                render: function (data, type, row) {
+                    if (row.Status == "For appraisal") {
+                        return '<td>' +
+                        '<span class="label label-outline-info">' + row.Status + '</span>'
+                        '<td>';
                     }
-                },
-                { data: "TransactionType", "className": "text-left" },
-                {
-                    className: "text-left",
-                    render: function (data, type, row) {
-                        return row.first_name + " " + row.last_name
+                    else if (row.Status == "For pawning") {
+                        return '<td>' +
+                        '<span class="label label-outline-warning">' + row.Status + '</span>'
+                        '<td>';
                     }
-                },
-                {
-                    data: "Status",
-                    className: "text-center",
-                    render: function (data, type, row) {
-                        if (row.Status == "For appraisal") {
-                            return '<td>' +
-                            '<span class="label label-outline-info">' + row.Status + '</span>'
-                            '<td>';
-                        }
-                        else if (row.Status == "For pawning") {
-                            return '<td>' +
-                            '<span class="label label-outline-warning">' + row.Status + '</span>'
-                            '<td>';
-                        }
-                        else if (row.Status == "Completed") {
-                            return '<td>' +
-                            '<span class="label label-outline-success">' + row.Status + '</span>'
-                            '<td>';
-                        }
-                        else {
-                            return '<td>' +
-                            '<span class="label label-outline-primary">' + row.Status + '</span>'
-                            '<td>';
-                        }
+                    else if (row.Status == "Completed") {
+                        return '<td>' +
+                        '<span class="label label-outline-success">' + row.Status + '</span>'
+                        '<td>';
                     }
-                },
-                {
-                    className: "text-center",
-                    render: function () {
-                        return '<div class="dropdown">' +
-                            '<button class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" type="button">' +
-                              'More '
-                            + '<span class="caret"></span>' +
-                            '</button>' +
-                            '<ul class="dropdown-menu dropdown-menu-right">' +
-                              '<li><a href="#">View</a></li>' +
-                              '<li><a href="' + RootUrl + '/administrator/appraisal">Go to appraisal</a></li>' +
-                              '<li role="separator" class="divider"></li>' +
-                              '<li><a href="#">Cancel transaction</a></li>' +
-                            '</ul>' +
-                          '</div>';
+                    else {
+                        return '<td>' +
+                        '<span class="label label-outline-primary">' + row.Status + '</span>'
+                        '<td>';
                     }
                 }
-                ]
-            });
-
-            $(window).on('resize', function (evt) {
-                setTimeout(function () {
-                    $datatablesFixedheader.columns.adjust().responsive.recalc();
-                }, 150);
-            });
-        }
+            },
+            {
+                className: "text-center",
+                render: function () {
+                    return '<div class="dropdown">' +
+                        '<button class="btn btn-primary btn-xs dropdown-toggle" data-toggle="dropdown" type="button">' +
+                          'More '
+                        + '<span class="caret"></span>' +
+                        '</button>' +
+                        '<ul class="dropdown-menu dropdown-menu-right">' +
+                          '<li><a href="#">View</a></li>' +
+                          '<li><a href="' + RootUrl + '/administrator/appraisal">Go to appraisal</a></li>' +
+                          '<li role="separator" class="divider"></li>' +
+                          '<li><a href="#">Cancel transaction</a></li>' +
+                        '</ul>' +
+                      '</div>';
+                }
+            }
+            ],
+            order: [[4, "desc"]]
+        });
     }
 
-
-
-
-
+    function NewTransactionPawning() {
+        isListShowed(false);
+        isCreateModeShow(true);
+        title("Pawning");
+    }
+    
     function backToList() {
-        loadTransactionList();
         isListShowed(true);
-        transactions(false);
-        createCustomer(false);
-
-        isCreateModeShowedPawning(false);
-
-        isSaveButtonShowed(false);
+        isCreateModeShow(false);
     }
 
 
@@ -167,8 +153,13 @@
         hasNewTransaction: hasNewTransaction,
 
         isListShowed: isListShowed,
+        isCreateModeShow: isCreateModeShow,
 
-        update: update
+        update: update,
+
+        NewTransactionPawning: NewTransactionPawning,
+        backToList: backToList,
+        title: title
     };
 
     return vm;
